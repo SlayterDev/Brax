3935740b7d510d954039af5e960164a4f1751d89
Brad Slayter <BradleySlayter@my.unt.edu>
2015-02-23 09:30:35 -0600

    Got the LED to blink! After learning how to return in ARM assmbly XD

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -2,13 +2,31 @@
 #include <stdint.h>
 
 extern void goLed();
+extern void noLed();
+extern void delay();
 
 int kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
 	(void)r0;
 	(void)r1;
 	(void)atags;
 
-	goLed();
+	//goLed();
+	uint8_t ledStatus = 0;
+	while (1) {
+		delay();
+	//	uint32_t cnt = 0;
+	//	if (cnt % 10000 == 0) {
+			if (ledStatus) {
+				noLed();
+				ledStatus = 0;
+			} else {
+				goLed();
+				ledStatus = 1;
+			}
+	//	}
+
+	//	cnt++;
+	}
 
 	while (1);
 
